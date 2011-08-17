d832d6b6d826b5b0dbc285d1b24b3a08ee06a264
Dragos <dragos@Dragos-Mirons-Mac-Pro.local>
2011-08-17 15:50:30 +0300

    first commit

---
+++ lib/time-freezer.rb
@@ -0,0 +1,7 @@
+require "time-freezer/version"
+
+module Time
+  module Freezer
+    # Your code goes here...
+  end
+end
---
+++ lib/time-freezer/version.rb
@@ -0,0 +1,5 @@
+module Time
+  module Freezer
+    VERSION = "0.0.1"
+  end
+end
